fafbf7965aebb4464d94e915b3bda9b29837d3b9
Vinit <vinitsk99@gmail.com>
2023-10-21 22:37:19 +0530

    Checking whether a number is even  or odd

---
+++ if-else.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = 7107;
+
+//Checking if the number is even or odd
+if ($num % 2 == 0)   //condition
+{
+    echo "$num is even.";
+}
+
+else{
+    echo "$num is odd.";
+}
+
+/* if-else statements in PHP allow us to execute specific blocks of code based on whether a given condition is true or false.
+    if given condition is true then the 'if' block will be execute otherwise 'else' block will execute;
+
+ * if-else statements can be nested inside each other, allowing us to create complex decision-making structures. */
+?>
