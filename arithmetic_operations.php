c7c4b1f080badcbad1e07b5d4115ea44a120777d
Vinit <vinitsk99@gmail.com>
2023-10-21 22:14:21 +0530

    Performing calculations

---
+++ arithmetic_operations.php
@@ -0,0 +1,39 @@
+<?php
+// Arithmetic operations in PHP
+
+// Define variables
+echo "1st number = ".$num1 = 2;
+echo "<br>2nd number = ".$num2 = 4;
+
+echo "<br><br>Various arithmetic operations: <br>";
+
+// Addition
+$sum = $num1 + $num2;
+echo "Sum: $sum<br>";
+
+// Subtraction
+$difference = $num1 - $num2;
+echo "Difference: $difference<br>";
+
+// Multiplication
+$product = $num1 * $num2;
+echo "Product: $product<br>";
+
+// Division
+$quotient = $num1 / $num2;
+echo "Quotient: $quotient<br>";
+
+// Modulus (remainder)
+$remainder = $num1 % $num2;
+echo "Remainder: $remainder<br>";
+
+// Exponentiation
+$exponentiation = $num1 ** $num2;
+echo "Exponentiation: $exponentiation<br>";
+
+
+/* Arithmetic operations in PHP allow you to perform basic mathematical calculations on numbers. 
+
+ * PHP supports various arithmetic operators to handle:
+   addition, subtraction, multiplication, division, modulus, and exponentiation.*/
+?>
