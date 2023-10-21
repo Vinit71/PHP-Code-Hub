df7d79312dc9842f3d4a7c73a0e670889216f91c
Vinit <vinitsk99@gmail.com>
2023-10-21 21:28:20 +0530

    Printing statements

---
+++ echo_and_print.php
@@ -0,0 +1,15 @@
+<?php
+
+/* echo is generally faster compared to print
+ * it does not have a return value. */
+echo "Welcome to PHP-Code-Hub <br>";
+
+// print always returns 1, so it can be used in expressions.
+
+print "Welcome but from 'print' ";
+
+/*echo and print are used for outputting data in PHP, 
+but echo is more versatile and slightly faster,
+while print is more suitable when you want to use its return value in expressions.*/
+?>
+
