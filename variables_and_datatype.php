a449710b070e32204a33cbf0176c4144721feee6
Vinit <vinitsk99@gmail.com>
2023-10-21 21:44:44 +0530

    Creating variables

---
+++ variables_and_datatype.php
@@ -0,0 +1,20 @@
+<?php
+$name = "Vinit";
+$age = 20;
+
+echo " Name: ".$name. "<br> Age: ". $age;
+/* --Variables--
+ * In PHP, variables are denoted by a dollar sign ($) followed by the variable name. 
+ 
+ * When naming variables in PHP, there are a few rules to follow:
+    Variable names must start with a letter or an underscore (_).
+    Variable names can only contain letters, numbers, and underscores.
+    Variable names are case-sensitive ($name and $Name are different variables).
+
+--Datatype--
+
+ * PHP is a dynamically typed language, which means you don't need to declare the data type of a variable when you create it.
+ * PHP automatically determines the data type based on the value assigned to the variable.
+ * PHP supports several basic data types, including integers, floats (floating-point numbers), strings, booleans, and null.
+*/
+?>
