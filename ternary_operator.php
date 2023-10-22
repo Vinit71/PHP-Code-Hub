21a328866a621e811fee03858a8430bb64a44b18
Vinit <vinitsk99@gmail.com>
2023-10-22 12:41:48 +0530

    Can you vote?

---
+++ ternary_operator.php
@@ -0,0 +1,13 @@
+<?php
+$age = 20;
+
+$Vote = ($age >= 18) ? "You can Vote" : "You cannot Vote"; // it's that simple 
+
+echo $canVote;
+
+/*
+ * The ternary operator in PHP is a shorthand method for writing simple if-else statements.
+ * The ternary operator works by evaluating a condition,
+   If the condition is true, it returns one value; if false, it returns another value. */
+?>
+
