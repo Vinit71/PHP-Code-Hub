cff0d118f2cb4178223431453166ae6086182844
Vinit <vinitsk99@gmail.com>
2023-10-22 13:22:33 +0530

    printing until condition is true

---
+++ while_loop.php
@@ -0,0 +1,10 @@
+<?php
+$number = 1;
+
+//while(condition){}
+while ($number <= 5) {
+    echo $number . "<br>";
+    $number++;
+}
+//A while loop repeatedly executes a block of code as long as a specified condition is true.
+?>
