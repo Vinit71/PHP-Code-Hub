acd44c85d14b29a7ac2ced5ac9a7f17b5094fdd4
Vinit <vinitsk99@gmail.com>
2023-10-22 13:10:46 +0530

    Series of even numbers only

---
+++ for_loop.php
@@ -0,0 +1,13 @@
+<?php
+echo "Even numbers between 1 and 20: ";
+/*
+    for(initialization, condition, and increment/decrement){
+        Statement
+    }
+*/
+for ($i = 2; $i <= 20; $i += 2) {
+    echo $i . " ";  //Printing even numbers only
+}
+
+//A for loop executes a block of code repeatedly for a fixed number of times.
+?>
