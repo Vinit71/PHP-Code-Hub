d66cb9c29240b53246c5b56835d763cbea688f55
Vinit <vinitsk99@gmail.com>
2023-10-25 22:47:13 +0530

    Special array

---
+++ associative_array.php
@@ -0,0 +1,15 @@
+<?php
+
+$stmarks = array("Vinit"=> "100", "Chaya" => "90", "Random" => "10"); //Associative array
+
+echo "Vinit got: ", $stmarks['Vinit'], " marks"; //printing
+
+/* Associative arrays store data as key-value pairs, where each key is unique and maps to a specific value.
+
+ * In an associative array, each key must be unique. If you attempt to insert a duplicate key, 
+   it typically results in updating the existing key-value pair with the new value.
+
+ * Associative arrays can store data of different types, eg: integers, strings, objects, or even other arrays.
+
+*/
+?>
