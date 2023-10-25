5244e37735814f602ecbe77c3db2d4a9787860da
Vinit <vinitsk99@gmail.com>
2023-10-25 23:55:26 +0530

    Printing greeting msg

---
+++ string_interpolation.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Vinit";
+
+$greeting = "Hello, $name!"; // Interpolated string
+
+echo $greeting; // Outputs: Hello, Vinit!
+
+/* String interpolation in PHP refers to the process of embedding variables directly into strings. 
+
+ * It allows variables to be directly placed within double-quoted strings.
+ 
+ * String interpolation improves code readability by simplifying the process of including variables in strings. */
+
+?>
