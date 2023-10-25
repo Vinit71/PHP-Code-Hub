c57ae82fdde47e85c429ebf297d7bee3c79cad73
Vinit <vinitsk99@gmail.com>
2023-10-25 23:46:22 +0530

    Special loop

---
+++ foreach_loop.php
@@ -0,0 +1,16 @@
+<?php
+$colors = array("Red", "Green", "Blue", "Yellow"); //indexed array
+
+// Using foreach to iterate over the indexed array
+echo "Colors in the array:<br>";
+foreach ($colors as $value) {
+    echo $value . "<br>";
+}
+
+/* foreach loop in PHP simplifies iterating through arrays.
+
+ * It automatically handles indexes. 
+  
+ * It makes code more readable and concise. */
+
+?>
