dd348d21650ce28d6fc6ed3d82cda1d95b0ccf0f
Vinit <vinitsk99@gmail.com>
2023-10-31 21:38:49 +0530

    comparing two values

---
+++ comparison_operators.php
@@ -0,0 +1,16 @@
+<?php
+
+// Comparison Operators
+$num1 = 10;
+$num2 = 5;
+
+echo "Comparison Operators:<br>";
+var_dump($num1 == $num2);  // Equal
+var_dump($num1 != $num2);  // Not Equal
+var_dump($num1 < $num2);   // Less than
+var_dump($num1 > $num2);   // Greater than
+var_dump($num1 <= $num2);  // Less than or equal to
+var_dump($num1 >= $num2);  // Greater than or equal to
+
+/*Comparison operators are used to compare two values and return a boolean result indicating the comparison's validity.*/
+?>
