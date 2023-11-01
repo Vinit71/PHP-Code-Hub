028e9093466c21cce4ef679c7cfa5201eb574a69
Vinit <vinitsk99@gmail.com>
2023-11-01 23:41:04 +0530

    print_r() function

---
+++ print_r.php
@@ -0,0 +1,12 @@
+<?php
+// Sample associative array
+$person = array(
+    "name" => "Ajay",
+    "age" => 30,
+    "city" => "Chandigarh"
+);
+
+// Printing the structure and values of the array using print_r()
+echo "Using print_r() function:<br>";
+print_r($person);
+?>
