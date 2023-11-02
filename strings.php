6385c4a5a6ceaf10bbf7b91e8f12f3513c0fb321
Vinit <vinitsk99@gmail.com>
2023-11-02 10:05:09 +0530

    Strings

--- strings.php
+++ strings.php
@@ -7,5 +7,5 @@ echo $str1;
 echo $str2;
 
 /* In PHP, a string is a sequence of characters enclosed within single quotes (') or double quotes (").
-    Strings are used to store and manipulate textual data in PHP programs. */
+   Strings are used to store and manipulate textual data in PHP programs. */
 ?>
