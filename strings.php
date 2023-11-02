195b32c977b5943e8636e8aad63346af21e7dbef
Vinit <vinitsk99@gmail.com>
2023-11-02 10:03:51 +0530

    Stings in php

---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+$str1 = "This is a String<br>";
+$str2 = "This is another String";
+
+//printing Strings
+echo $str1;
+echo $str2;
+
+/* In PHP, a string is a sequence of characters enclosed within single quotes (') or double quotes (").
+    Strings are used to store and manipulate textual data in PHP programs. */
+?>
